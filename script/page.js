e2a55b90e6ada3a18cccd7e8439f9e52e4cc520e
JIRAPAT_144 <59030144@kmitl.ac.th>
2019-05-24 01:57:31 +0700

    add friend

--- script/page.js
+++ script/page.js
@@ -1,3 +1,4 @@
+var slideIndex = 1;
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -11,3 +12,18 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
  
+
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
