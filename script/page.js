ef22cb61c3f01ce8e9be328687861ea93ca3143a
JIRAPAT_144 <59030144@kmitl.ac.th>
2019-05-25 01:11:27 +0700

    add information friend

--- script/page.js
+++ script/page.js
@@ -1,15 +1,13 @@
 var slideIndex = 1;
-var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700){
     document.getElementById("navbar").style.top = "0";
     document.getElementById("navbar").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
-  } else {
-    document.getElementById("navbar").style.top = "-60px";
+  }
+  else{
+    document.getElementById("navbar").style.top = "-80px";
     document.getElementById("navbar").style.boxShadow = "none";
   }
-  prevScrollpos = currentScrollPos;
 }
  
 
@@ -19,7 +17,7 @@ function plusDivs(n) {
 }
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("mySlide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
